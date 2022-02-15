d53d2d4bc83364d69ff12d4e8048fd689ad0f6ba
DusDus3428 <dustinnoah.young@gmail.com>
2022-02-15 16:49:15 +0100

    Improved project structure by using Spring Initializr and renaming the front end project

---
+++ back-end-spring-security-example/src/main/java/com/edataconsulting/medium/backendspringsecurityexample/BackEndSpringSecurityExampleApplication.java
@@ -0,0 +1,13 @@
+package com.edataconsulting.medium.backendspringsecurityexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndSpringSecurityExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndSpringSecurityExampleApplication.class, args);
+	}
+
+}
---
+++ back-end-spring-security-example/src/test/java/com/edataconsulting/medium/backendspringsecurityexample/BackEndSpringSecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edataconsulting.medium.backendspringsecurityexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndSpringSecurityExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
